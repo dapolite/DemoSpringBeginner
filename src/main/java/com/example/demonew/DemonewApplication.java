a7045fa660261aa9fb18de634fa6e51cd7ff2c3b
dapolite <ankurbanerjeeofficial@gmail.com>
2020-02-10 16:43:35 +0530

    1st Commit Spring Demo Adds,Updates,Delete a person name as JSON Object from Postman Has an artificial database?

---
+++ src/main/java/com/example/demonew/DemonewApplication.java
@@ -0,0 +1,15 @@
+package com.example.demonew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan
+@SpringBootApplication
+public class DemonewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemonewApplication.class, args);
+	}
+
+}
